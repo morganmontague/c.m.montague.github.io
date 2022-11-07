0f256ba40f9c2f4cc2ce19603751a563ed30c121
C. Morgan Montague <morgan.montague@student.awesomeinc.org>
2022-11-07 02:00:09 +0000

    added accordian for the blogs

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,7 @@ import AlbumCard from "./components/album_card";
 import Button from "./components/button";
 import background from './IMG/ticToc.png';
 import background2 from './IMG/weather.png';
+import SingleAccordion from "./components/accordian";
 
 
 
@@ -15,6 +16,15 @@ function App() {
          <AlbumCard Title={'Weather App'} gitLink={"https://morganmontague.github.io/sandbox-weather/"} imgBack={background2} />
          <AlbumCard Title={'Read my Mind'} gitLink={"https://morganmontague.github.io/sandbox-read-my-mind/"} imgBack={background} />
       </div>
+      <div className="accordion accordion-flush" id="blogAccordian">
+         <SingleAccordion accordionTitle = {"Week 1"} accordionText={"Testing1"} />
+         <SingleAccordion accordionTitle = {"Week 2"} accordionText={"Testing2"} />
+         <SingleAccordion accordionTitle = {"Week 3"} accordionText={"Testing3"} />
+         <SingleAccordion accordionTitle = {"Week 4"} accordionText={"Testing4"} />
+         <SingleAccordion accordionTitle = {"Week 5"} accordionText={"Testing5"} />
+         <SingleAccordion accordionTitle = {"Week 6"} accordionText={"Testing6"} />
+         <SingleAccordion accordionTitle = {"Week 7"} accordionText={"What are some similarities and differences you can see with JS and Python. If you had started with Python instead of JS, how do you think the Bootcamp would be different? Would you have benefited? Think of a project or tech based tool that you use often and think about what the database structure might look like for it. Do you feel like you have a better understanding of how it works now and could you see yourself enjoying building something like that in the future? "} />
+      </div> 
    </div> 
 
     )
---
+++ app/src/components/accordian.js
@@ -0,0 +1,22 @@
+
+
+function SingleAccordion({accordionTitle, accordionText}) {
+    return(
+<div className="accordion accordion-flush" id="accordionFlushExample">
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="flush-headingOne">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+        {accordionTitle}
+      </button>
+    </h2>
+    <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+      <div className="accordion-body">{accordionText}</div>
+    </div>
+  </div>
+  
+</div>
+    )
+}
+
+export default SingleAccordion
+
