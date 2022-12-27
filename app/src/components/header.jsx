4bceca3602fe0d5e6b21f8c56883c2da54f66643
C. Morgan Montague <morgan.montague@student.awesomeinc.org>
2022-12-27 16:30:26 +0000

    added new components

---
+++ app/src/components/header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    <>
+        <h1>
+            This will be the Title of the portfolio
+        </h1>
+        <h4>
+            By Morgan Montague, <a>Github </a> and <a>LinkedIn</a>
+
+        </h4>
+    </>
+
+}
+
+export default Header
---
+++ app/src/components/info.jsx
@@ -0,0 +1,15 @@
+function InfoArea () {
+    return (
+        <div className="container">
+            <h1>{Title}</h1>
+            <div>
+                <br></br>
+                <div>
+                {Description}
+                </div>
+                <br></br>
+            </div>
+        </div>    
+    )
+}
+export default InfoArea
